66562505f9d465ddce2fbc754160afeab22bc694
RoskiDeluge <roberto.delgado@gmail.com>
2022-06-27 09:19:40 -1000

    Added model: content-filter-alpha

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -1,4 +1,10 @@
 import { NUMBER_URL, POEM_URL } from '../config/index';
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 export async function getCurrentNumber() {
   const response = await fetch(`${NUMBER_URL}/prod/number?id=atomicCounter`);
@@ -8,7 +14,7 @@ export async function getCurrentNumber() {
 
   return number;
 }
-// TODO: Need to add content filter function on poemgenerator lambda
+
 export async function getCurrentPoem() {
   const options = {
     method: 'POST',
@@ -22,5 +28,28 @@ export async function getCurrentPoem() {
 
   const poem = data.choices[0].text;
 
-  return poem;
+  const filterResponse = await openai.createCompletion({
+    model: 'content-filter-alpha',
+    prompt: '< endoftext|>' + poem + '\n--\nLabel:',
+    temperature: 0,
+    max_tokens: 1,
+    top_p: 1,
+    logprobs: 10,
+  });
+
+  const filterLabel = filterResponse.data.choices[0].text;
+  const unsafe =
+    'Potentially unsafe result generated. Will try again in 24hrs.';
+
+  if (filterLabel == '0' || filterLabel == '1') {
+    // res.status(200).json({
+    //   text: '${content_to_classify}',
+    // });
+    return poem;
+  } else {
+    // res.status(200).json({ text: 'Try again, after modifying the prompt.' });
+    return unsafe;
+  }
+
+  // return poem;
 }
