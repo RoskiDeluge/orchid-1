d68a0d2b1aeb4a56f05a7b2bd646f12b2e42aeae
RoskiDeluge <roberto.delgado@gmail.com>
2022-11-03 11:04:23 -1000

    Changing to stablediffusion

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // reactStrictMode: true,
-  images: {
-    domains: ['firebasestorage.googleapis.com'],
+// const nextConfig = {
+//   // reactStrictMode: true,
+//   images: {
+//     domains: ['firebasestorage.googleapis.com'],
+//   },
+// };
+
+// module.exports = nextConfig;
+
+module.exports = {
+  reactStrictMode: false,
+  webpack: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+
+    return config;
   },
 };
-
-module.exports = nextConfig;
