4bf731ce4cfe5f5db36e544089243d442408137c
RoskiDeluge <roberto.delgado@gmail.com>
2022-11-21 10:14:07 -1000

    edited readme

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function Header() {
   return (
     <header className="p-5 bg-blue-500">
-        <p>Orchid Zero</p>
+        <p>Orchid Zero | <Link href="https://github.com/RoskiDeluge/orchid-0" target="_blank">About</Link></p>
     </header>
   )
 }
